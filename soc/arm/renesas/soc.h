cf2d8f84434d2dfeb37a36dd202e6d9490ed7f1b
Julien Massot <julien.massot@iot.bzh>
2020-07-21 15:49:52 +0200

    soc: add basic definition for Renesas Cortex R7
    Renesas Gen3 boards H3, M3, E3 have an embbeded Cortex-r7.

---
+++ soc/arm/renesas/soc.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2020 IoT.bzh
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+#ifndef _BOARD__H_
+#define _BOARD__H_
+
+/* Define CMSIS configurations */
+#define __CR_REV		1U
+
+#endif /* _BOARD__H_ */
