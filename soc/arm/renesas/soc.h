20a36bcfcabe078b5ee002f3c132b3589b0b77c1
Julien Massot <julien.massot@iot.bzh>
2021-02-04 09:32:12 +0100

    rcar_cr7: use core_cr7 instead of core_cr5
    No longer rely on Cortex R5 compatibility.
    
    Signed-off-by: Julien Massot <julien.massot@iot.bzh>

--- soc/arm/renesas/soc.h
+++ soc/arm/renesas/soc.h
@@ -11,4 +11,8 @@
 /* Define CMSIS configurations */
 #define __CR_REV		1U
 
+/* Do not let CMSIS to handle GIC an Timer */
+#define __GIC_PRESENT 0
+#define __TIM_PRESENT 0
+
 #endif /* _BOARD__H_ */
