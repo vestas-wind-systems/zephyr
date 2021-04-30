b88157b4ed3426e51cf526da6c3cc0f646cb73bf
Julien Massot <julien.massot@iot.bzh>
2021-04-30 15:26:33 +0200

    board: arm: rcar_h3ulcb: add pinmux configuration
    Set the pinmux configuration for possible function
    CAN or SCIF.
    
    Signed-off-by: Julien Massot <julien.massot@iot.bzh>

---
+++ boards/arm/rcar_h3ulcb/pinmux.c
@@ -0,0 +1,31 @@
+/* Copyright 2021 IoT.bzh
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <init.h>
+#include <soc.h>
+#include <pinmux/pinmux_rcar.h>
+
+static int rcar_ulcb_pinmux_init(const struct device *dev)
+{
+	ARG_UNUSED(dev);
+
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(can0), okay) && CONFIG_CAN
+	pinmux_rcar_set_pingroup(can0_data_a, ARRAY_SIZE(can0_data_a));
+#endif
+
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(scif1), okay) && CONFIG_SERIAL
+	/* SCIF1 = SECONDARY SERIAL PORT */
+	pinmux_rcar_set_pingroup(scif1_data_a, ARRAY_SIZE(scif1_data_a));
+#endif
+
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(scif2), okay) && CONFIG_SERIAL
+	/* SCIF2 = UBOOT OUTPUT */
+	pinmux_rcar_set_pingroup(scif2_data_a, ARRAY_SIZE(scif2_data_a));
+#endif
+
+	return 0;
+}
+
+SYS_INIT(rcar_ulcb_pinmux_init, PRE_KERNEL_1, CONFIG_PINMUX_INIT_PRIORITY);
